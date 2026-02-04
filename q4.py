3a4901f25d15fecaf566ed56a2296d2ebc210b5f
comrn0n <eike.miranda@acad.ifma.edu.br>
2026-02-04 19:12:47 -0300

    Update q4

--- q4.py
+++ q4.py
@@ -4,4 +4,4 @@ x = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
 
 covariancia = np.cov(x)
 
-print("Matriz de covariância aproximada: ", covariancia)
+print("Matriz de covariância aproximada:", covariancia)
